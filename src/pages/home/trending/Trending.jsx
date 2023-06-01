05e78bfa4ead4f7efb1ab9083e2e7407ccb4cfce
Adarsh-Kumbar-in <adarshkumbarbgm@gmail.com>
2023-06-01 17:54:30 +0530

    Rating and Stuff

--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -2,13 +2,14 @@ import { useState } from "react";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
 import useFetch from "../../../hooks/useFetch";
+import Carousel from "../../../components/carousel/Carousel";
 
 //css for all carousal is in Home style.scss
 
 const Trending = () => {
-  const [endpoint, setEndpoint] = useState("day");  //send this to api call
+  const [endpoint, setEndpoint] = useState("day"); //send this to api call
 
-    const { data , loading} = useFetch(`/trending/all/${endpoint}`)
+  const { data, loading } = useFetch(`/trending/all/${endpoint}`);
 
   //for every tab change we call API
   const onTabChange = (tab) => {
@@ -21,6 +22,7 @@ const Trending = () => {
         <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} />
         {/* above we can render more things to like year month etc && 1st onTabChange is prop and 2nd one is method we created*/}
       </ContentWrapper>
+        <Carousel data={data?.results} loading ={loading} />
     </div>
   );
 };
