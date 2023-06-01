c2776841cd6949a07235bb77e3ba89e35eee6907
Adarsh-Kumbar-in <adarshkumbarbgm@gmail.com>
2023-06-02 02:25:16 +0530

    Added trailer
    used ReactPlayer component from react-player/youtube ...which takes some arguments like url .Controls are provided to control the video

---
+++ src/components/videoPopup/VideoPopup.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactPlayer from "react-player/youtube"; // library of react
+
+import "./style.scss";
+
+//got these props from Detailsbanner.jsx
+const VideoPopup = (props) => {
+    const { show, setShow, videoId, setVideoId } = props;
+    const hidePopup = () => {
+    setShow(false);
+    setVideoId(null);
+  };
+  return (
+    <div className={`videoPopup ${show ? "visible" : ""}`}>
+      <div className="opacityLayer" onClick={hidePopup}></div>
+      <div className="videoPlayer">
+        <span className="closeBtn" onClick={hidePopup}>
+          Close
+        </span>
+        <ReactPlayer
+          url={`https://www.youtube.com/watch?v=${videoId}`}
+          controls
+          width="100%"
+          height="100%"
+          playing={true}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default VideoPopup;
--- src/pages/details/detailsBanner/DetailsBanner.jsx
+++ src/pages/details/detailsBanner/DetailsBanner.jsx
@@ -13,8 +13,13 @@ import Img from "../../../components/lazyLoadImage/Img.jsx";
 import PosterFallback from "../../../assets/no-poster.png";
 import CircleRating from "../../../components/cicleRating/CircleRating";
 import { PlayIcon } from "../Playbtn";
+import VideoPopup from "../../../components/videoPopup/VideoPopup";
 
 const DetailsBanner = ({ video, crew }) => {
+const[show,setShow] = useState(false);
+const[videoId,setVideoId] = useState(null);
+
+
   const { mediaType, id } = useParams();
   const { data, loading } = useFetch(`/${mediaType}/${id}`);
 
@@ -76,7 +81,10 @@ const DetailsBanner = ({ video, crew }) => {
 
                     <div className="row">
                       <CircleRating rating={data.vote_average.toFixed(2)} />
-                      <div className="playbtn">
+                      <div className="playbtn" onClick={()=>{
+                        setShow(true);
+                        setVideoId(video.key)
+                      }}>
                         <PlayIcon />
                         <span className="text">Watch Trailer</span>
                       </div>
@@ -170,10 +178,16 @@ const DetailsBanner = ({ video, crew }) => {
                           ))}
                         </span>
                       </div>
-                    )}
+                    )} 
 
                   </div>
                 </div>
+                <VideoPopup
+                show={show}
+                setShow={setShow}
+                videoId={videoId}
+                setVideoId={setVideoId} />
+
               </ContentWrapper>
             </>
           )}
